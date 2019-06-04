8d669894d66afa891bc519620c313dc2cbab43e3
nsatosh <nakatsukasatoshi@gmail.com>
2019-06-04 14:04:11 +0900

    Add tests of ModifySelectionCommand

---
+++ tests/commands/ModifySelectionCommand.test.ts
@@ -0,0 +1,105 @@
+import { ModifySelectionCommand } from "../../src/core/commands/ModifySelectionCommand";
+import { invokeCommand } from "../../src/core/EditorCommand";
+import EditorMutator from "../../src/core/EditorMutator";
+import NodeMap from "../../src/core/NodeMap/NodeMap";
+import { U } from "../../src/core/U";
+
+test("Do nothing when cursor is not enabled", () => {
+  const nodeMap = new NodeMap({});
+  nodeMap.createRootNode("root");
+  nodeMap.appendChild("root", U.end("te"));
+  let editor = EditorMutator.createExistingEditorState(nodeMap, "root");
+
+  editor = invokeCommand(
+    new ModifySelectionCommand({ id: "te", ch: 0 }),
+    editor
+  );
+
+  expect(editor).toBe(editor);
+});
+
+test("Make selection that covers backward character", () => {
+  const nodeMap = new NodeMap({});
+  nodeMap.createRootNode("root");
+  nodeMap.appendChild("root", U.text("t0", "0123"));
+  nodeMap.appendChild("root", U.end("te"));
+  let editor = EditorMutator.createExistingEditorState(nodeMap, "root");
+
+  editor.cursorAt = { id: "t0", ch: 2 };
+
+  editor = invokeCommand(
+    new ModifySelectionCommand({ id: "t0", ch: 1 }),
+    editor
+  );
+
+  expect(editor.selection).toEqual({
+    start: { id: "t0", ch: 1 },
+    end: { id: "t0", ch: 2 },
+    focus: "start"
+  });
+});
+
+test("Make selection that covers forward character", () => {
+  const nodeMap = new NodeMap({});
+  nodeMap.createRootNode("root");
+  nodeMap.appendChild("root", U.text("t0", "0123"));
+  nodeMap.appendChild("root", U.end("te"));
+  let editor = EditorMutator.createExistingEditorState(nodeMap, "root");
+
+  editor.cursorAt = { id: "t0", ch: 2 };
+
+  editor = invokeCommand(
+    new ModifySelectionCommand({ id: "r1t1", ch: 3 }),
+    editor
+  );
+
+  expect(editor.selection).toEqual({
+    start: { id: "r1t1", ch: 2 },
+    end: { id: "r1t1", ch: 3 },
+    focus: "end"
+  });
+});
+
+test("Make selection that covers backward node", () => {
+  const nodeMap = new NodeMap({});
+  nodeMap.createRootNode("root");
+  nodeMap.appendChild("root", U.text("t0", "abc"));
+  nodeMap.appendChild("root", U.text("t1", "def"));
+  nodeMap.appendChild("root", U.end("te"));
+  let editor = EditorMutator.createExistingEditorState(nodeMap, "root");
+
+  editor.cursorAt = { id: "t1", ch: 1 };
+
+  editor = invokeCommand(
+    new ModifySelectionCommand({ id: "t0", ch: 2 }),
+    editor
+  );
+
+  expect(editor.selection).toEqual({
+    start: { id: "t0", ch: 2 },
+    end: { id: "t1", ch: 1 },
+    focus: "start"
+  });
+});
+
+test("Make selection that covers forward node", () => {
+  const nodeMap = new NodeMap({});
+  nodeMap.createRootNode("root");
+  nodeMap.appendChild("root", U.text("t0", "abc"));
+  nodeMap.appendChild("root", U.text("t1", "def"));
+  nodeMap.appendChild("root", U.end("te"));
+  let editor = EditorMutator.createExistingEditorState(nodeMap, "root");
+
+  editor.cursorAt = { id: "t0", ch: 2 };
+
+  editor = invokeCommand(
+    new ModifySelectionCommand({ id: "t1", ch: 1 }),
+    editor
+  );
+
+  expect(editor.selection).toEqual({
+    start: { id: "t0", ch: 2 },
+    end: { id: "t1", ch: 1 },
+    focus: "end"
+  });
+});
