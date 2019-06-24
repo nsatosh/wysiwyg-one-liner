bf7d5c5a98ce1c1edf336d96d150c6eeed2f608e
nsatosh <nakatsukasatoshi@gmail.com>
2019-06-24 13:22:36 +0900

    Add link node example

---
+++ example/link/index.tsx
@@ -0,0 +1,111 @@
+import React, { FC } from "react";
+import { render } from "react-dom";
+import { Content } from "../../src/component/Content";
+import { EditorMutator, NodeMap, TETextNode, TEBaseNode, TENodeID, TENodeMap, TELeafNode, TESentinelNode } from "../../src/core";
+import { U } from "../../src/core/U";
+import { NodeSchema } from "../../src/core/NodeSchema";
+import InlineText from "../../src/component/node/InlineText";
+import InlineSentinel from "../../src/component/node/InlineSentinel";
+import { BUILTIN_ITEMS } from "../../src/core/BuiltinNodeSchema";
+import { CustomNodeProps } from "../../src/component/CustomNodeProps";
+
+interface TELinkNode extends TEBaseNode {
+  type: "link";
+
+  children: TENodeID[];
+
+  url: string;
+
+  parent: TENodeID;
+}
+
+const InlineLink: FC<CustomNodeProps<TELinkNode>> = props => {
+  const { node, editor } = props;
+  const { nodeMap, inDebug } = editor;
+
+  const nodes = node.children.reduce<TELeafNode[]>((nodes, id) => {
+    const node = nodeMap[id];
+
+    if (!node) {
+      console.warn(`invalid child found: ${id}`);
+      return nodes;
+    }
+
+    nodes.push(node as TELeafNode);
+
+    return nodes;
+  }, []);
+
+  return (
+    <a href={node.url} target="_blank">
+      {nodes.map(node => {
+        if (node.type === "text") {
+          return (
+            <InlineText
+              key={node.id}
+              inDebug={inDebug}
+              node={node as TETextNode}
+            />
+          );
+        }
+
+        if (node.type === "sentinel") {
+          return (
+            <InlineSentinel
+              key={node.id}
+              node={node as TESentinelNode}
+              inDebug={inDebug}
+            />
+          );
+        }
+      })}
+    </a>
+  );
+};
+
+const CustomTextNode: FC<CustomNodeProps<TETextNode>> = props => {
+  const { node } = props;
+
+  return <InlineText node={node} />
+};
+
+const Editor: FC = () => {
+  let nodeSchema = new NodeSchema([
+    ...BUILTIN_ITEMS,
+    {
+      type: "link",
+      category: "internal",
+      isBlockNode: false,
+      isInlineContainerNode: true,
+      getLength: () => undefined,
+      getText: () => undefined,
+      canHaveCursor: false
+    }
+  ]);
+  nodeSchema = nodeSchema.setCustomNodeComponent("text", CustomTextNode);
+  nodeSchema = nodeSchema.setCustomNodeComponent("link", InlineLink);
+
+  const nodeMap = new NodeMap(nodeSchema, {});
+  nodeMap.createRootNode("root");
+
+  const { id } = nodeMap.appendChild<TELinkNode>("root", {
+    type: "link",
+    url: "https://example.com"
+  });
+  nodeMap.appendChild(id, U.sentinel("s0"));
+  nodeMap.appendChild(id, U.text("t2", "A link to example.com"));
+  nodeMap.appendChild(id, U.sentinel("s1"));
+  nodeMap.appendChild("root", U.end("te"));
+
+  const editor = EditorMutator.createFromNodeMap(nodeMap, "root");
+
+  return (
+    <div style={{ width: 800, height: 600 }}>
+      <Content defaultValue={editor} />
+    </div>
+  );
+};
+
+window.addEventListener("load", function () {
+  render(<Editor />, document.getElementById("root"));
+});
