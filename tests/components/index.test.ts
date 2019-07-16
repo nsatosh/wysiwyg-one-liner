f6ba6ebd5b65e0f2d8f28f8e692380c0da0a4f63
nsatosh <nakatsukasatoshi@gmail.com>
2019-07-16 13:31:34 +0900

    Fix test

--- tests/components/index.test.ts
+++ tests/components/index.test.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const FIRST_LINE = { x: 200, y: 55 };
+const FIRST_LINE = { x: 200, y: 10 };
 const CENTER = { x: 200, y: 200 };
 
 const SCREENSHOT_DIR = "tmp";
