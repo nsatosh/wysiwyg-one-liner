d89762b97f389fab449f9fc18585fcb7f5b9910c
nsatosh <nakatsukasatoshi@gmail.com>
2019-07-17 16:29:38 +0900

    Add sentinel node to builtin node schema

--- src/core/BuiltinNodeSchema.ts
+++ src/core/BuiltinNodeSchema.ts
@@ -39,129 +39,136 @@ export const SentinelNodeSchema: NodeSchemaItem = {
   canHaveCursor: true
 };
 
+export const RowNodeSchema: NodeSchemaItem = {
+  type: "row",
+  category: "root",
+  isBlockNode: true,
+  isInternalNode: false,
+  getLength: () => undefined,
+  getText: () => undefined,
+  canHaveCursor: false
+};
+
+export const EndNodeSchema: NodeSchemaItem = {
+  type: "end",
+  category: "leaf",
+  isBlockNode: false,
+  isInternalNode: false,
+  getLength: () => 1,
+  getText: () => undefined,
+  delete: (
+    nodeMap: NodeMap,
+    node: TETextNode,
+    _range: OpenableRange,
+    stat: Stat
+  ) => {
+    switch (stat) {
+      case Stat.before:
+      case Stat.after:
+      case Stat.closed:
+      case Stat.opened:
+        return;
+      case Stat.between:
+        nodeMap.deleteNode(node.id, true);
+        return;
+      case Stat.single:
+      default:
+        throw new Error("Unexpected condition");
+    }
+  },
+  component: InlineEnd,
+  canHaveCursor: true
+};
+
+export const TextNodeSchema: NodeSchemaItem = {
+  type: "text",
+  category: "leaf",
+  isBlockNode: false,
+  isInternalNode: false,
+  getLength: (node: TETextNode) => node.text.length,
+  getText: (node: TETextNode) => node.text,
+  textPositionToCoord: (
+    el,
+    node: TETextNode,
+    eOffset: ElementOffset,
+    ch: number
+  ): Coord => {
+    return {
+      top: eOffset.top,
+      left: eOffset.left + calcTextWidth(el, node, ch)
+    };
+  },
+  coordToTextPosition: (el, node: TETextNode, coord) => {
+    const mouseX = coord.left - el.getBoundingClientRect()!.left;
+    let left = 0;
+    let right = el.innerText.length;
+
+    while (right - left > 1) {
+      const center = Math.floor((left + right) / 2);
+
+      if (calcTextWidth(el, node, center) < mouseX) {
+        left = center;
+      } else {
+        right = center;
+      }
+    }
+
+    const centerX =
+      (calcTextWidth(el, node, left) + calcTextWidth(el, node, right)) / 2;
+
+    return {
+      id: node.id,
+      ch: mouseX < centerX ? left : right,
+      nonCanonical: true
+    };
+  },
+  delete: (
+    nodeMap: NodeMap,
+    node: TETextNode,
+    range: OpenableRange,
+    stat: Stat
+  ) => {
+    const startCh = range.start.ch;
+    const endCh = range.end.ch;
+    const { id, text } = node;
+
+    switch (stat) {
+      case Stat.before:
+      case Stat.after:
+        return;
+
+      case Stat.between:
+        nodeMap.deleteNode(id, true);
+        return;
+
+      case Stat.single:
+        nodeMap.updateText(
+          id,
+          text.slice(0, startCh).concat(text.slice(endCh))
+        );
+        return;
+
+      case Stat.opened:
+        nodeMap.updateText(id, text.slice(0, startCh));
+        return;
+
+      case Stat.closed:
+        nodeMap.updateText(id, text.slice(endCh));
+        return;
+
+      default:
+        throw new Error("Unexpected condition");
+    }
+  },
+  component: InlineText,
+  canHaveCursor: true
+};
+
 export const BUILTIN_ITEMS: NodeSchemaItem[] = [
-  {
-    type: "row",
-    category: "root",
-    isBlockNode: true,
-    isInternalNode: false,
-    getLength: () => undefined,
-    getText: () => undefined,
-    canHaveCursor: false
-  },
-  {
-    type: "end",
-    category: "leaf",
-    isBlockNode: false,
-    isInternalNode: false,
-    getLength: () => 1,
-    getText: () => undefined,
-    delete: (
-      nodeMap: NodeMap,
-      node: TETextNode,
-      _range: OpenableRange,
-      stat: Stat
-    ) => {
-      switch (stat) {
-        case Stat.before:
-        case Stat.after:
-        case Stat.closed:
-        case Stat.opened:
-          return;
-        case Stat.between:
-          nodeMap.deleteNode(node.id, true);
-          return;
-        case Stat.single:
-        default:
-          throw new Error("Unexpected condition");
-      }
-    },
-    component: InlineEnd,
-    canHaveCursor: true
-  },
-  {
-    type: "text",
-    category: "leaf",
-    isBlockNode: false,
-    isInternalNode: false,
-    getLength: (node: TETextNode) => node.text.length,
-    getText: (node: TETextNode) => node.text,
-    textPositionToCoord: (
-      el,
-      node: TETextNode,
-      eOffset: ElementOffset,
-      ch: number
-    ): Coord => {
-      return {
-        top: eOffset.top,
-        left: eOffset.left + calcTextWidth(el, node, ch)
-      };
-    },
-    coordToTextPosition: (el, node: TETextNode, coord) => {
-      const mouseX = coord.left - el.getBoundingClientRect()!.left;
-      let left = 0;
-      let right = el.innerText.length;
-
-      while (right - left > 1) {
-        const center = Math.floor((left + right) / 2);
-
-        if (calcTextWidth(el, node, center) < mouseX) {
-          left = center;
-        } else {
-          right = center;
-        }
-      }
-
-      const centerX =
-        (calcTextWidth(el, node, left) + calcTextWidth(el, node, right)) / 2;
-
-      return {
-        id: node.id,
-        ch: mouseX < centerX ? left : right,
-        nonCanonical: true
-      };
-    },
-    delete: (
-      nodeMap: NodeMap,
-      node: TETextNode,
-      range: OpenableRange,
-      stat: Stat
-    ) => {
-      const startCh = range.start.ch;
-      const endCh = range.end.ch;
-      const { id, text } = node;
-
-      switch (stat) {
-        case Stat.before:
-        case Stat.after:
-          return;
-
-        case Stat.between:
-          nodeMap.deleteNode(id, true);
-          return;
-
-        case Stat.single:
-          nodeMap.updateText(
-            id,
-            text.slice(0, startCh).concat(text.slice(endCh))
-          );
-          return;
-
-        case Stat.opened:
-          nodeMap.updateText(id, text.slice(0, startCh));
-          return;
-
-        case Stat.closed:
-          nodeMap.updateText(id, text.slice(endCh));
-          return;
-
-        default:
-          throw new Error("Unexpected condition");
-      }
-    },
-    component: InlineText,
-    canHaveCursor: true
-  }
+  SentinelNodeSchema,
+  RowNodeSchema,
+  EndNodeSchema,
+  TextNodeSchema
 ];
 
 function calcTextWidth(
