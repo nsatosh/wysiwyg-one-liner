9c3251d8a4b69441c03c87c928bff8a9a413d296
nsatosh <nakatsukasatoshi@gmail.com>
2019-07-18 14:17:22 +0900

    Remove styling from InlineText

--- src/component/node/InlineText.tsx
+++ src/component/node/InlineText.tsx
@@ -10,24 +10,8 @@ const InlineText: FC<CustomNodeProps<TETextNode>> = memo(props => {
     node,
     editor: { inDebug }
   } = props;
-  const { bold, italic, strikethrough, underline } = node.style;
 
-  const style = {
-    whiteSpace: "pre",
-    fontWeight: bold ? "bold" : 300,
-    fontStyle: italic ? "italic" : "normal",
-    fontSize: "var(--TextFontSize)",
-    textDecoration: [
-      underline ? "underline" : "",
-      strikethrough ? "line-through" : ""
-    ].join(" ")
-  } as React.CSSProperties;
-
-  return (
-    <span ref={ref} style={style}>
-      {node.text.join("") || (inDebug ? "□" : " ")}
-    </span>
-  );
+  return <span ref={ref}>{node.text.join("") || (inDebug ? "□" : " ")}</span>;
 });
 
 export default InlineText;
