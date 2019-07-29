f04d8f6a019ee1ff011d746ad575105aa5028619
nsatosh <nakatsukasatoshi@gmail.com>
2019-07-29 14:00:44 +0900

    Add example

---
+++ example/tagSearch/index.tsx
@@ -0,0 +1,101 @@
+import React, { FC, useCallback } from "react";
+import { render } from "react-dom";
+import {
+  EditorMutator,
+  NodeMap,
+  NodeSchema,
+  BUILTIN_ITEMS,
+  SentinelNodeType,
+  TETextNode,
+  TELeafNode,
+  CustomNodeProps,
+  InlineText,
+  InlineSentinel,
+  Input,
+  TEInternalNode,
+  TEEditor
+} from "../../src";
+import { NodeSchemaItem } from "../../src/core/NodeSchema";
+
+interface TagNode extends TEInternalNode {
+  type: "tag";
+}
+
+const InlineTag: FC<CustomNodeProps<TagNode>> = props => {
+  const { node, editor } = props;
+  const { nodeMap, inDebug, nodeSchema } = editor;
+
+  const nodes = node.children.reduce<TELeafNode[]>((nodes, id) => {
+    const node = nodeMap[id];
+
+    if (!node) {
+      console.warn(`invalid child found: ${id}`);
+      return nodes;
+    }
+
+    nodes.push(node as TELeafNode);
+
+    return nodes;
+  }, []);
+
+  return (
+    <span style={{ padding: 5, border: "1px solid black" }}>
+      {nodes.map(node => {
+        if (nodeSchema.isTextNode(node)) {
+          return <InlineText key={node.id} node={node} editor={editor} />;
+        }
+
+        if (node.type === SentinelNodeType) {
+          return <InlineSentinel key={node.id} node={node} inDebug={inDebug} />;
+        }
+      })}
+    </span>
+  );
+};
+
+const TAG_SCHEMA: NodeSchemaItem = {
+  type: "tag",
+  category: "internal",
+  isBlockNode: false,
+  isInternalNode: true,
+  getLength: () => undefined,
+  getText: () => undefined,
+  canHaveCursor: false,
+  component: InlineTag
+};
+
+function initializeEditorState() {
+  const nodeSchema = new NodeSchema([...BUILTIN_ITEMS, TAG_SCHEMA]);
+
+  const nodeMap = new NodeMap(nodeSchema, {});
+  nodeMap.createRootNode("root");
+
+  const { id } = nodeMap.appendChild<TagNode>("root", {
+    type: "tag"
+  });
+  nodeMap.appendChild(id, { type: SentinelNodeType });
+  nodeMap.appendChild<TETextNode>(id, {
+    type: "text",
+    text: "tag1".split("")
+  });
+  nodeMap.appendChild(id, { type: SentinelNodeType });
+  nodeMap.appendChild("root", {
+    type: "end"
+  });
+
+  return EditorMutator.createFromNodeMap(nodeMap, "root");
+}
+
+const INITIAL_EDITOR = initializeEditorState();
+
+const Editor: FC = () => {
+  return (
+    <div style={{ width: 800, height: 600 }}>
+      <Input defaultValue={INITIAL_EDITOR} />
+    </div>
+  );
+};
+
+window.addEventListener("load", function() {
+  render(<Editor />, document.getElementById("root"));
+});
